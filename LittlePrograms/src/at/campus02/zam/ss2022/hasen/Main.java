ac02358e164c6192bdc39e8a44fdb57f73211e97
Christian Hofer <christian.hofer@campus02.at>
2022-04-27 09:48:14 +0200

    Little Programs

--- LittlePrograms/src/at/campus02/zam/ss2022/hasen/Main.java
+++ LittlePrograms/src/at/campus02/zam/ss2022/hasen/Main.java
@@ -3,7 +3,7 @@ package at.campus02.zam.ss2022.hasen;
 public class Main {
     public static void main(String[] args) {
         Osterhase oster = new Osterhase("Bugs");
-        Weihnachshase weihnachshase = new Weihnachshase("Snowi");
+        Weihnachtshase weihnachshase = new Weihnachtshase("Snowi");
 
         oster.hoppeln();
         weihnachshase.hoppeln();
@@ -29,6 +29,55 @@ public class Main {
         hs.addHase(weihnachshase);
 
         hs.hoppelAll();
+
+
+
+        // try around with up and downcasting
+        Osterhase o1 = new Osterhase("Mrs. Egg");
+        Osterhase o2 = o1;
+        Hase h1 = new Hase("Common rabbit (but cute)");
+        Weihnachtshase w2 = new Weihnachtshase("Mr. Snow");
+
+
+        Hase h4 = new Osterhase("Bunny");
+
+
+
+        Weihnachtshase wh;
+        Osterhase oh;
+        Hase h;
+
+        // vererbungshierarchie
+        //                   |--->  Osterhase
+        // Object --> Hase ---
+        //                   |--->  Weihnachtshase
+
+        // upcasting: assigning a child class to a reference of its partent class
+        // example:
+        // THIS IS UPCASTING
+        h = o1;  // assign from right (child class Osterhase) to left (parent class Hase)
+        // what is the difference if i acces now my Mrs. Egg via the h Hase referernce
+        // -> the versteckeOstereier is missing because we only see attributes and methods
+        // already the Hase has (imagine: we view the Osterhase Mrs. Egg through the lens of a Hase)
+        // BUT THE NICE THING IS: if we call a method already known to the Hase then the correc implementaion
+        // (the nearest one) is executed -> so in our case the implementation of the Osterhase
+        h.hoppeln();
+
+
+        // why do make this? -> because of poloymorphism (think about arraylist)
+
+        // now what is DOWN CASTING?
+        // assigning a general type to a more specialized type (ATTENTION - ALARM SIGNAL)
+        // this only works if this general reference already refers to a specialized object instance
+        //wh = (Weihnachtshase) h1; // this gives us a ClassCastException
+        // but there are special cases when down casting works (i will have to upcast before)
+        h = w2; // this happened somewhere before (could be years)
+
+        // but we know that behind this hase reference is really a Weihnachtshase
+        // (we know it from somewhere - next time we will also learn how to check)
+        wh = (Weihnachtshase) h;
+        h.hoppeln();
+        wh.hoppeln();
 
     }
 }
---
+++ LittlePrograms/src/at/campus02/zam/ss2022/hasen/Weihnachtshase.java
@@ -0,0 +1,17 @@
+package at.campus02.zam.ss2022.hasen;
+
+public class Weihnachtshase extends Hase {
+    public Weihnachtshase(String n) {
+        super(n);
+    }
+
+    @Override
+    public void hoppeln() {
+        System.out.println("Weihnachtshase " + name + " wälzt sich durch den Schnees und bringt Geschenke");
+    }
+    public void verteileGeschenke(){
+        System.out.println(name + " gräbt sich durch den Boden und verteilt Geschenke - hätte bei Albuquerque links abbiegen");
+    }
+
+
+}
